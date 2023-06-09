4696c24363e873b5eb8639b905d496c114e5070d
vagrant <vagrant@linux-vag20>
2023-06-09 15:01:06 +0000

    Fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-for i in range(11):
+for i in range(100):
     for j in range(i + 1):
-        if j % 10 == i // 10 and j // 10 == i % 10:
+        if j // 10 == 0:
+            if j % 10 == i // 10 and i % 10 == 0:
+                continue
+        elif j % 10 == i // 10 and j // 10 == i % 10:
             continue
     if i == 99:
         print("{:02d}".format(i))
