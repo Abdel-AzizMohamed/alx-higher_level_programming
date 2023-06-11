530e85c8e31ae314a43967ce6dd4b612c51d2b22
vagrant <vagrant@linux-vag20>
2023-06-11 06:10:40 +0000

    Add fix args length

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    args_length = len(sys.argv)
+    args_length = len(sys.argv) - 1
     if args_length != 3:
         print("./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
