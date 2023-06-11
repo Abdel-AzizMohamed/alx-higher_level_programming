51e57c9f999cccd24cb0ac7aaceaa4a9503f060f
vagrant <vagrant@linux-vag20>
2023-06-11 04:39:11 +0000

    Add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+
+    if len(args) == 1:
+        print("{:d} argument.".format(0))
+    else:
+        print("{:d} argument:".format(len(args)))
+
+    for i, item in enumerate(args):
+        if i == 0:
+            continue
+        print("{:d}: {}".format(i, item))
