73f0d7cb2ffd2fb0d131f81d71d55345c78acd0a
vagrant <vagrant@linux-vag20>
2023-06-11 05:12:41 +0000

    Add task 4 file type

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
 
-for item in dir(hidden_4):
-    if item[0] == "_":
-        continue
-    else:
-        print(item)
+if __name__ == "__main__":
+    import hidden_4
+
+    for item in dir(hidden_4):
+        if item[0] == "_":
+            continue
+        else:
+            print(item)
