04a40ac7a7e55ea44307822c5a8216205f2ebf4b
vagrant <vagrant@linux-vag20>
2023-06-12 19:29:59 +0000

    Fix task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
+    if len(matrix) == 1 and len(matrix[0]) == 0:
         print()
     for row in matrix:
         for count, column in enumerate(row):
