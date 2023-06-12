3c750682a5afcfab44550fe51ca43a84bd42687a
vagrant <vagrant@linux-vag20>
2023-06-12 19:27:22 +0000

    Fix task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        print()
     for row in matrix:
         for count, column in enumerate(row):
             if count + 1 == len(row):
                 print("{:d}".format(column))
             else:
                 print("{:d}".format(column), end=" ")
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
