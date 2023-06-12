c0f5367e6602affa7cd30b9efd3ea127c24284c7
vagrant <vagrant@linux-vag20>
2023-06-12 11:55:20 +0000

    Add task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    i = 0
+    while i + 1 < len(my_list):
+        if my_list[i + 1] >= max:
+            max = my_list[i + 1]
+        i += 1
+    return max
