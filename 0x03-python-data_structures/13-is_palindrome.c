75ad1aaea019758a8c88c653e989beb528d3ec8d
vagrant <vagrant@linux-vag20>
2023-06-15 09:04:18 +0000

    Fix empty head

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,10 +10,16 @@ int is_palindrome(listint_t **head)
 	int *numbers_list, len, i, j;
 	listint_t *node = *head;
 
+	if (!head)
+		return (1);
+
 	for (len = 0; node; len++)
 		node = node->next;
 
 	numbers_list = malloc(sizeof(int) * len);
+
+	if (!numbers_list)
+		return (NULL);
 
 	node = *head;
 
