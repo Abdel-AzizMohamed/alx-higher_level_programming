e915060a44de303ab0da7a37b4ea2462fdf576a3
vagrant <vagrant@linux-vag20>
2023-06-15 08:40:06 +0000

    Fix betty for task 13 in project 0x03

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,27 +7,27 @@
 */
 int is_palindrome(listint_t **head)
 {
-    int *numbers_list, len, i, j;
-    listint_t *node = *head;
+	int *numbers_list, len, i, j;
+	listint_t *node = *head;
 
-    for (len = 0; node; len++)
-        node = node->next;
+	for (len = 0; node; len++)
+		node = node->next;
 
-    numbers_list = malloc(sizeof(int) * len);
+	numbers_list = malloc(sizeof(int) * len);
 
-    node = *head;
+	node = *head;
 
-    for (i = 0; node; i++)
-    {
-        numbers_list[len] = node->n;
-        node = node->next;
-    }
+	for (i = 0; node; i++)
+	{
+		numbers_list[len] = node->n;
+		node = node->next;
+	}
 
-    for (i = 0, j = len; i < len / 2; i++, j--)
-    {
-        if (numbers_list[i] != numbers_list[j - 1])
-            return (0);
-    }
+	for (i = 0, j = len; i < len / 2; i++, j--)
+	{
+		if (numbers_list[i] != numbers_list[j - 1])
+			return (0);
+	}
 
-    return (1);
+	return (1);
 }
