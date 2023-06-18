27c3c68b06602cc41a7ad9ea37e0af3dfcf32ced
vagrant <vagrant@linux-vag20>
2023-06-18 05:16:32 +0000

    Add task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+	keys_list = []
+	for key, item in a_dictionary.items():
+		if value == item:
+			keys_list.append(key)
+	for item in keys_list:
+		del a_dictionary[item]
+	new_dict = a_dictionary.copy()
+	return new_dict
