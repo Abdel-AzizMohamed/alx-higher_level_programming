fa1727b549ada08877040494152275a462eab01c
vagrant <vagrant@linux-vag20>
2023-06-18 04:47:49 +0000

    Add task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total_score = 0
+    total_weight = 0
+    for item in my_list:
+        total_score += item[0] * item[1]
+        total_weight += item[1]
+    return total_score / total_weight
