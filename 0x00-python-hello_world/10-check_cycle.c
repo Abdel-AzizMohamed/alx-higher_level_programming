130f46ff196a0f12576b074a3cf20ef2a3d97035
vagrant <vagrant@linux-vag20>
2023-06-26 15:09:29 +0000

    Add Task 0

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - check if the given list has cycle
+ * @list: given linked list
+ * Return: 1 if there is a cycle else 0
+*/
+int check_cycle(listint_t *list)
+{
+	return (1);
+}
