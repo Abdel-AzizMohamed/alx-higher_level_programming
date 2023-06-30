37f53292fd4ad9d73d3de657fe5dd233916ce32f
vagrant <vagrant@linux-vag20>
2023-06-30 17:38:37 +0000

    Add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""define a square object"""
+
+
+class Square():
+    """Represent A Square"""
+    def __init__(self, size):
+        """Initialize a new object
+
+        Args:
+            size (int): the size of square object
+        """
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
