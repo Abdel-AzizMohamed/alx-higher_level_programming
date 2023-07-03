618fa4a7c73aeed2d028881a4aeeb3997e276150
vagrant <vagrant@linux-vag20>
2023-07-03 01:31:42 +0000

    Add task 101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""define a square object"""
+
+
+class Square():
+    """Represent A Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new object
+
+        Args:
+            size (int): the size of square object
+        """
+        self.size = size
+        self.position = position
+
+    def __str__(self): 
+        return "{}".format(self.my_print(False))
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self, last=True):
+        if self.size == 0:
+            print("")
+            return 0
+
+        for _ in range(self.position[1]):
+            print("")
+
+        for i in range(self.size):
+            for _ in range(self.position[0]):
+                print(" ", end="")
+            for _ in range(self.size):
+                print("#", end="")
+            if not last and self.size == i + 1:
+                break
+            print("")
+        return ""
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) != int or type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
