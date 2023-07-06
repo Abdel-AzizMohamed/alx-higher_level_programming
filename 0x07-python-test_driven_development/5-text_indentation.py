b3c5a68886219edf22bb19ef6b8a54e7c2edc324
vagrant <vagrant@linux-vag20>
2023-07-06 16:30:30 +0000

    Fix last char

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -21,11 +21,19 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     i = 0
     while (i < len(text)):
+        print(text[i], end="")
         if text[i] in ".?:":
             if text[i + 1] == " ":
                 i += 1
             print("\n")
-            i += 1
-            continue
-        print(text[i], end="")
         i += 1
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
