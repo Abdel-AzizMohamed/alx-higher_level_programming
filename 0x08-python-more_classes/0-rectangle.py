e4757910546e968e028cea2ecece678bc900e5cc
vagrant <vagrant@linux-vag20>
2023-07-08 13:42:19 +0000

    Add task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a rectangle class
+
+    Classes:
+        Rectangle
+"""
+
+
+class Rectangle():
+    """Define a Rectangle Object"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+# Python - More Classes and Objects Project
+
+## Description
+-------
+this project for learing more about classes in python
