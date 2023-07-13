07afa2568e5189545155c4904cb7c7cec3283fe1
vagrant <vagrant@linux-vag20>
2023-07-13 18:54:43 +0000

    Fix style

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,7 +19,14 @@ class BaseGeometry():
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
+    """Define Rectangle Object"""
     def __init__(self, width, height):
+        """intalize a new rect object
+
+            Args:
+                width(positive int): rect width
+                height(positive int): rect height
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
