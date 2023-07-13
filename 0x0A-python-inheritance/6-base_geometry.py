bf047f61efec04f593d3bfecec031f8f08ac5df1
vagrant <vagrant@linux-vag20>
2023-07-13 17:39:15 +0000

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define Empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Define BaseGeomatry Object"""
+    def area(self):
+        raise Exception("area() is not implemented")
