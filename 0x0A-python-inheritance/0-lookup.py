432d40804d6af0ea8da82333731eaaa8594f97fc
vagrant <vagrant@linux-vag20>
2023-07-13 14:07:27 +0000

    Add task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a object lockup function
+
+	Functions:
+		lookup(object)
+"""
+
+
+def lookup(obj):
+	"""returns the list of attributes and methods of an object"""
+	return dir(obj)
