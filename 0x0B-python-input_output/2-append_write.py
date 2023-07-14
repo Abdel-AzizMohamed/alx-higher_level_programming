e071b214059c19fa6b302ce17090c32d8b67dfdc
vagrant <vagrant@linux-vag20>
2023-07-14 16:09:44 +0000

    Add task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""Define a file append function
+
+
+    Functions:
+        append_file(filename="", text="")
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="UTF-8") as a:
+        char_count = a.write(text)
+    return char_count
