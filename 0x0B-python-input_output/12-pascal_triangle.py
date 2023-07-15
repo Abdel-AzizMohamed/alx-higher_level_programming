a04a23546e40dc25a278a8c54e18a314037192d9
vagrant <vagrant@linux-vag20>
2023-07-15 10:38:21 +0000

    Add task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    pas_list = []
+
+    if n <= 0:
+        return []
+
+    pas_list.append([1])
+
+    for i in range(n - 1):
+        row_list = []
+        for j in range(i+2):
+            if j == 0:
+                row_list.append(1)
+            elif j == i + 1:
+                row_list.append(1)
+            else:
+                row_list.append(pas_list[i][j] + pas_list[i][j - 1])
+        pas_list.append(row_list)
+    return pas_list
