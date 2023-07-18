ec80cc182f3548db82e69dc883b365fcafa14604
vagrant <vagrant@linux-vag20>
2023-07-18 01:16:32 +0000

    Add tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a base class"""
+
+
+class Base():
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if not id:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
