1508c869236e59c6cf70dc4f991d16d7529a93fb
vagrant <vagrant@linux-vag20>
2023-07-18 01:26:25 +0000

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,11 @@
 
 
 class Base():
+    """Define a base object"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """init a new object"""
         if not id:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Define a new rect object"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """init a new rect object"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -83,13 +85,13 @@ class Rectangle(Base):
         return str_rep.format(self.id, x, y, wd, hi)
 
     def update(self, *args):
-       if len(args) >= 1:
-            self.id = args[0] 
-       if len(args) >= 2:
-            self.__width = args[1] 
-       if len(args) >= 3:
-            self.__height = args[2] 
-       if len(args) >= 4:
-            self.__x = args[3] 
-       if len(args) >= 5:
-            self.__y = args[4] 
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
