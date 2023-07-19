5c52bf949dec0a26762e6b7492b05cf0ca80aad2
vagrant <vagrant@linux-vag20>
2023-07-19 11:21:04 +0000

    Add task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,6 +39,7 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
         if not json_string:
             return []
         return json.loads(json_string)
