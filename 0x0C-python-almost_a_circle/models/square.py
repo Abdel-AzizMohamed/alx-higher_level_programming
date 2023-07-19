10b7f501718d3cea802447fbd2005ad8d5cd3f50
vagrant <vagrant@linux-vag20>
2023-07-19 08:37:40 +0000

    Fix task 11 style

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,10 +4,20 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Define square object"""
     def __init__(self, size, x=0, y=0, id=None):
+        """initalize a new object
+
+            Args:
+                size(int): size of the square
+                x(int): x-axis of the square
+                y(int): y-axis of the square
+                id(int): id of the square
         super().__init__(size, size, x, y, id)
+        """
 
     def __str__(self):
+        """return a string reperstintion of square object"""
         ob_id = self.id
         ob_x = self.x
         ob_y = self.y
