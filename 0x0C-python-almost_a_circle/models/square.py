62d761a81c56e0efa6546530156c4534c8bf7b52
vagrant <vagrant@linux-vag20>
2023-07-19 08:54:54 +0000

    Add task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,3 +33,24 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """update object attributes"""
+        if not args:
+            for key, value in kwargs.items():
+                if key == "size":
+                    setattr(self, "width", value)
+                    setattr(self, "height", value)
+                else:
+                    setattr(self, key, value)
+            return (0)
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+            self.height = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -16,3 +16,29 @@ class TestRectangle_init(unittest.TestCase):
     def test_str(self):
         sq_1 = Square(10, 2, 4, 3)
         self.assertEqual(str(sq_1), "[Square] (3) 2/4 - 10")
+
+    def test_setter(self):
+        sq_1 = Square(1, 1, 1)
+        sq_1.size = 15
+        sq_1.x = 2
+        sq_1.y = 4
+        self.assertEqual(sq_1.width, 15)
+        self.assertEqual(sq_1.height, 15)
+        self.assertEqual(sq_1.size, 15)
+        self.assertEqual(sq_1.x, 2)
+        self.assertEqual(sq_1.y, 4)
+
+    def test_update(self):
+        sq_1 = Square(1, 1, 1)
+        sq_1.update(10, 15, 3, 4) 
+        self.assertEqual(sq_1.width, 15)
+        self.assertEqual(sq_1.height, 15)
+        self.assertEqual(sq_1.size, 15)
+        self.assertEqual(sq_1.x, 3)
+        self.assertEqual(sq_1.y, 4)
+        sq_1.update(size=10, x=15, y=3) 
+        self.assertEqual(sq_1.width, 10)
+        self.assertEqual(sq_1.height, 10)
+        self.assertEqual(sq_1.size, 10)
+        self.assertEqual(sq_1.x, 15)
+        self.assertEqual(sq_1.y, 3)
