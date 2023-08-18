da4ab221aa107e49266cf273942f5791f36b0e6c
vagrant <vagrant@linux-vag20>
2023-08-18 16:22:14 +0000

    Fix task 0 & 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates a user that have all premssions
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1@localhost;
