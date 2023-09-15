7f5be4d7f7d3045cb5e0fcca3f145ae9b4ebac02
vagrant <vagrant@linux-vag20>
2023-09-15 15:16:19 +0000

    Fix task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareP = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareP {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
-    let ch = c;
-    if (!ch) {
-      ch = 'X';
+    if (!c) {
+      c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      console.log(ch.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
