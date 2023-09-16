c52f4fe5f4c1b9ae4e4d23ba5a789991bf452bdd
vagrant <vagrant@linux-vag20>
2023-09-16 13:49:04 +0000

    Add task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,17 +4,17 @@ Create a connect to mysql server by useing MySQLdb
 and fetch some data from it
 """
 from MySQLdb import connect
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    db = connect(host="localhost", user=argv[1], passwd=argv[2],
-                 db=argv[3], port=3306)
+    db = connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+                 db=sys.argv[3], port=3306)
 
     cr = db.cursor()
 
     cr.execute("""SELECT * FROM states ORDER BY id ASC""")
-    fetched_data = data.fetchall()
+    fetched_data = cr.fetchall()
     for row in fetched_data:
         print(row)
     cr.close()
