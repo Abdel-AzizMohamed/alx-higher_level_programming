9ff5681034277daff0e52c6470d7627102f3fa7c
vagrant <vagrant@linux-vag20>
2023-09-18 16:26:09 +0000

    Add task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Select all objects from State table"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for obj in session.query(State).order_by(State.id):
+        if obj.name.find("a") != -1:
+            print("{}: {}".format(obj.id, obj.name))
