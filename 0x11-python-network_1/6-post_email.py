907fb6a26dbe18cd0f285be2ad3170493817d228
vagrant <vagrant@linux-vag20>
2023-09-29 12:53:31 +0000

    Fix task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import sys
 
 if __name__ == "__main__":
     post = {"email": argv[2]}
-    req = requests.get(sys.argv[1], params=post)
+    req = requests.post(sys.argv[1], params=post)
 
     print(req.text)
