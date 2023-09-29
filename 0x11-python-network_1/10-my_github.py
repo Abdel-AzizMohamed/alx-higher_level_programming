7b9665c8083288e425a0035f73dc273806db60c1
vagrant <vagrant@linux-vag20>
2023-09-29 13:11:52 +0000

    Add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""display your github id"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
