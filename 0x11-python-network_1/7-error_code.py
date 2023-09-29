1a21b395853be8131aac137c73eb30e98720eb42
vagrant <vagrant@linux-vag20>
2023-09-29 12:52:43 +0000

    Add task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     req = requests.get(url)
 
     if req.status_code >= 400:
-        print("Error code: {}".format(status_code))
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
