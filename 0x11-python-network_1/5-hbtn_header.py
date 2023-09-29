13955cdb43d2ba358a91d112c58069904a4167e4
vagrant <vagrant@linux-vag20>
2023-09-29 12:29:31 +0000

    Add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""displays the value of the variable X-Request-Id by requests lib"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+
+    print(req.headers["X-Request-Id"])
