5b529d3367ff14a943d0480ed5ab48216b0a3d45
vagrant <vagrant@linux-vag20>
2023-09-29 13:13:25 +0000

    Fix task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """display your github id"""
 import requests
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == '__main__':
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
